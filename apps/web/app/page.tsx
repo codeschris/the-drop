ceac6e918fafc70b84612192ef00c3401371585f
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2026-03-10 06:54:51 +0000

    reduce width size for mobile screens

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl font-black">[the drop.] | Coming soon!</h1>
       <img src="assets/pepe-dj.gif" alt="cooking up cool sesh"/>
-      <p className="text-center p-8 md:p-20 w-1/2">
+      <p className="text-center p-5 md:p-20 md:w-1/2">
         A platform curated by <span className="font-black italic">mwml</span> and <span className="font-black italic">Josh, The Curator</span> to bring artists and their communities together to embrace the art of music and community listening parties
         when albums and tracks are released.
       </p>
